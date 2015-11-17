c95c5029905b05e1b3504b2ca8d254bfcf2c938d
sisidovski <arerreee@gmail.com>
2015-11-17 11:37:07 +0900

    add start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sudo xcodebuild -license
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew doctor
+brew update
+
+brew install python
+brew install ansible
+
+if [ -e ~/.osx-provisioning ]; then
+  mkdir ~/.osx-provisioning
+fi
+
+cd ~/.osx-provisioning/
+
+echo 'localhost' > hosts
+
+HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook -i hosts -vv localhost.yml
