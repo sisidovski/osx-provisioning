7a6678ae44152f155650075791aa099c1a48d5b4
sisidovski <arerreee@gmail.com>
2015-11-17 18:46:10 +0900

    add karabiner setting

---
+++ karabiner-import.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set repeat.wait 25
+/bin/echo -n .
+$cli set repeat.initial_wait 180
+/bin/echo -n .
+$cli set remap.pointing_relative_middleclick_to_scroll 1
+/bin/echo -n .
+$cli set remap.optionrcommandr 1
+/bin/echo -n .
+$cli set remap.expose2capslockORshiftL 1
+/bin/echo -n .
+/bin/echo
